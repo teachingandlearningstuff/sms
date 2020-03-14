214e353644f37c5ae9e342ce06abc82fd7e31fa3
Daniel Despain <danield@teachingstuff.com>
2020-03-14 13:34:36 -0700

    initial commit

---
+++ farm.php
@@ -0,0 +1,65 @@
+<html>
+	<head>
+		<title>Farm Theme from CTP</title>
+
+		<style>
+			div {
+				border: 2px solid black;
+				padding: 4px;
+				margin: 1em 0;
+			}
+
+			.imageBox {
+				width: 340px;
+				height: 120px;
+				border: 1px solid blue;
+				background-color: #ccc;
+				text-align: center;
+				font-size: 2em;
+				font-weight: bold;
+				padding-top: 40px;
+				margin: 0 auto;
+			}
+		</style>
+	</head>
+
+<body>
+
+<h1>Farm Theme is Cool</h1>
+
+<p>Check out the galery:</p>
+
+<div>
+	<div class="imageBox">IN USE<br />IMAGE 1</div>
+	<p>uses these items:</p>
+	<ul>
+		<li>CTP1</li>
+		<li>CTP2</li>
+		<li>BT Paper wood</li>
+	</ul>
+</div>
+
+<div>
+	<div class="imageBox">IN USE<br />IMAGE 2</div>
+	<p>uses these items:</p>
+	<ul>
+		<li>CTP11</li>
+		<li>CTP13</li>
+		<li>CTP4</li>
+		<li>CTP5</li>
+		<li>CTP6</li>
+	</ul>
+</div>
+
+<div>
+	<div class="imageBox">IN USE<br />IMAGE 3</div>
+	<p>uses these items:</p>
+	<ul>
+		<li>CTP10</li>
+		<li>black butcher paper</li>
+	</ul>
+</div>
+
+</body>
+
+</html>
